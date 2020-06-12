762b9fad3c1756c3860c06d36a6eeac4f73354c1
Edison Costa <edison@agiuscloud.com>
2020-06-12 12:16:18 -0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+VERSION=$1
+
+cd /tmp
+wget http://apt.puppetlabs.com/puppet$VERSION-release-$(lsb_release -sc).deb
+dpkg -i puppet$VERSION-release-$(lsb_release -sc).deb
+apt-get update
+
+apt-get -y install puppet-agent
+
+if ! echo $PATH | grep -q /opt/puppetlabs/bin ; then
+  export PATH=$PATH:/opt/puppetlabs/bin
+fi
+      
+mkdir -p /etc/facter/facts.d
